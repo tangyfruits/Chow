88b2ae38e02360e301984d260a15972b68b0c08a
Donald Tang <tangyfruits@gmail.com>
2016-06-02 00:35:35 +1200

    Associated recipes with users

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,9 @@
 class Article < ActiveRecord::Base
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "default.jpeg"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  belongs_to :user
   has_many :comments, dependent: :destroy
   validates :title, :text, presence: true,
                     length: { minimum: 1 }
+
 end
